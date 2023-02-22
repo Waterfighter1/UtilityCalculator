fa41b97ab0833d4ed15196bcc3c7c993edd30490
Chloe <chloe.gertner.cg@gmail.com>
2023-02-22 01:50:06 -0600

    Added Bar Widget

--- app/src/main/java/com/example/utilitycalculator/MainActivity.java
+++ app/src/main/java/com/example/utilitycalculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Button tipButton = (Button) findViewById(R.id.btn_tip);
         Button gasButton = (Button) findViewById(R.id.btn_gas);
         Button taxButton = (Button) findViewById(R.id.btn_tax);
+        Button rateButton = (Button) findViewById(R.id.btn_rate);
+        RatingBar rateBar = (RatingBar) findViewById(R.id.ratingBar);
 
         tipButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -33,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
         taxButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, tax.class));
+            }
+        });
+
+        rateButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                rateBar.setNumStars(0);
             }
         });
     }
